6a3345a42b3c3e8f594e5e2cd43f37af27138cfe
SOMYUUKTHAA <30828746+SOMYUUKTHAA@users.noreply.github.com>
2018-02-10 21:22:15 +0530

    Update Som3333.c

--- Som3333.c
+++ Som3333.c
@@ -1,6 +1,5 @@
-
 #include<stdio.h>
-main()
+void main()
 {
   char c;
   printf("Enter a character to know whether it is vowel or not\n");
@@ -8,5 +7,5 @@ main()
   if(c=='A'||c=='a'||c=='e'||c=='E'||c=='I'||c=='i'||c=='O'||c=='o'||c=='U'||c=='u')
     printf("The character %c is vowel\n",c);
   else
-    printf("The character %c is not vowel\n",c);
+    printf("The character %c is constant\n",c);
 }
